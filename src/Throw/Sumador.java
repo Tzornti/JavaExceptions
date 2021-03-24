eac7cff32bf6837579be04ee255de03274f95420
Jordy <alejandre030902@estudiantec.cr>
2021-03-24 12:26:56 -0600

    Creando repositorio

---
+++ src/Throw/Sumador.java
@@ -0,0 +1,21 @@
+package Throw;
+import java.util.Scanner;
+
+public class Sumador {
+    public static void main(String[] args) {
+        var num1 = 0;
+        var num2 = 0;
+        Scanner escrito = new Scanner(System.in);
+        System.out.println("Ingrese el primer numero");
+        num1 = escrito.nextInt();
+        System.out.println("Ingrese el segundo numero");
+        num2 = escrito.nextInt();
+        Sumando primer= new Sumando(num1,num2);
+        try {
+            primer.suma();
+        }
+        catch (IllegalArgumentException negative){
+            System.err.println(negative.getMessage());
+        }
+    }
+}
---
+++ src/Throw/Sumando.java
@@ -0,0 +1,23 @@
+package Throw;
+
+public class Sumando {
+    //Clase que suma enteros positivos y hace Throw en caso de recibir un negativo
+    int number1=0;
+    int number2=0;
+    int resultado;
+    public Sumando(int x,int y){
+        number1=x;
+        number2=y;
+    }
+    public int suma(){
+        if (this.number2<0 || this.number1<0){
+            throw new IllegalArgumentException("No se admiten valores negativos.");
+        }
+        else{
+            resultado=this.number1+this.number2;
+            return resultado;
+        }
+    }
+
+}
+
